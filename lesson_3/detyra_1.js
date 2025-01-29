921f565a51d3d8aaaecdc0793c779df95865141b
krenar4code <ajdinikrenar1@gmail.com>
2025-01-29 19:25:47 +0100

    init

---
+++ lesson_3/detyra_1.js
@@ -0,0 +1,36 @@
+// // Program per llogaritjen e kohes
+// let distanca = parseFloat(prompt("Jepni distance ne (km):"));
+// let shpejtesia = parseFloat(prompt("Jepni shpejtesine ne (km/h):"));
+
+// if (shpejtesia > 0) {
+//   let koha = distanca / shpejtesia;
+//   console.log(('Koha per te pershkuar distancen eshte: ${koha.toFixed(2)} orë'));
+  
+// } else {
+//   console.log("Shpejtesia duhet te jete me e madhe se zero!");
+// }
+
+
+
+// // Program per llogaritjen e diametrit dhe siperfaqes se rrethit
+// let rrezja = parseFloat(prompt("Jepni rrezen e rrethit:"));
+
+// let diametri = 2 * rrezja;
+// let siperfaqja = Math.PI * rrezja ** 2;
+
+// console.log(`Diametri i rrethit eshte: ${diametri.toFixed(2)}`);
+// console.log(`Siperfaqja e rrethit eshte: ${siperfaqja.toFixed(2)}`);
+
+
+// // Kurset e kembimit
+// let kursiEuro = 90; // 1 Euro = 90 Lek
+// let kursiDollar = 100; // 1 Dollar = 100 Lek
+
+// // Program per konvertimin e lekeve ne Euro dhe Dollar
+// let sasiaLek = parseFloat(prompt("Jepni sasine e parave ne Lek:"));
+
+// let sasiaEuro = sasiaLek / kursiEuro;
+// let sasiaDollar = sasiaLek / kursiDollar;
+
+// console.log((`Sasia ne Euro eshte: ${sasiaEuro.toFixed(2)} EUR`));
+// console.log((`Sasia ne Dollar eshte: ${sasiaDollar.toFixed(2)} USD`));
