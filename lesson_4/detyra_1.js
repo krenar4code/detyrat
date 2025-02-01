760af9601a0eed5c6227a7e807f339ed0e7f382f
krenar4code <ajdinikrenar1@gmail.com>
2025-02-01 17:43:50 +0100

    init

--- lesson_4/detyra_1.js
+++ lesson_4/detyra_1.js
@@ -1,18 +1,15 @@
-// // Marrja e dy numrave nga perdoruesi
-// let n1 = parseFloat(prompt("Shkruani numrin e pare:"));
-// let n2 = parseFloat(prompt("Shkruani numrin e dyte:"));
+// Marrja e dy numrave nga perdoruesi
+let n1 = parseFloat(prompt("Shkruani numrin e pare:"));
+let n2 = parseFloat(prompt("Shkruani numrin e dyte:"));
 
-// // Kontrolli dhe veprimi 
-// if (n1 > n2) {
-//     console.log("Shuma:", n1 + n2);
-//     console.log("Shuma: " + (n1 + n2));
-// } else if (n2 > n1) {
-//     console.log("Prodhimi:", n1 * n2);
-//     console.log("Prodhimi: " + (n1 * n2));
-// } else {
-//     console.log("Numrat jane te barabarte!");
-//     console.log("Numrat jane te barabarte!");
-// }
+// Kontrolli dhe veprimi 
+if (n1 > n2) {
+    console.log("Shuma:", n1 + n2);
+} else if (n2 > n1) {
+    console.log("Prodhimi:", n1 * n2);
+} else {
+    console.log("Numrat jane te barabarte!");
+}
 
 
 
@@ -22,9 +19,7 @@
 // // Kontrolli dhe printimi i rezultatit
 // if (mosha > 60) {
 //     console.log("Pensionist");
-//     console.log("Pensionist");
 // } else {
-//     console.log("Punetor");
 //     console.log("Punetor");
 // }
 
@@ -36,11 +31,8 @@
 // // Krahason emrat ne rend alfabetik
 // if (emri1.toLowerCase() < emri2.toLowerCase()) {
 //     console.log("Emri qe vjen i pari ne rend alfabetik eshte:", emri1);
-//     console.log("Emri qe vjen i pari ne rend alfabetik eshte: " + emri1);
 // } else if (emri1.toLowerCase() > emri2.toLowerCase()) {
 //     console.log("Emri qe vjen i pari ne rend alfabetik eshte:", emri2);
-//     console.log("Emri qe vjen i pari ne rend alfabetik eshte: " + emri2);
 // } else {
-//     console.log("Emrat jane te njejte!");
 //     console.log("Emrat jane te njejte!");
 // }
